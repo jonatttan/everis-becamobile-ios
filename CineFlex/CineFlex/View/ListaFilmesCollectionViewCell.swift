1664d0cffee05ece18d930f8fa87c932bab9b6d1
Jonattan <jonattan.sousa@gmail.com>
2021-03-28 19:57:31 -0300

    Exibindo imagem com link estático para verificar UI.

--- CineFlex/CineFlex/View/ListaFilmesCollectionViewCell.swift
+++ CineFlex/CineFlex/View/ListaFilmesCollectionViewCell.swift
@@ -9,13 +9,43 @@ import UIKit
 
 class ListaFilmesCollectionViewCell: UICollectionViewCell {
     
+    let url = "https://www.themoviedb.org/t/p/w440_and_h660_face/jhIm9MS8sfzsW8MAxmyrFv5o6t7.jpg"
+    
     //MARK: - IBOutlets
     
-    @IBOutlet weak var labelNome: UILabel!
+    @IBOutlet weak var imageCapa: UIImageView!
+    
     
     func formatCelula(_ filme:Filme){
         
-        labelNome.text = filme.title
+        guard let urlImagem = URL(string: "https://www.themoviedb.org/t/p/w440_and_h660_face/jhIm9MS8sfzsW8MAxmyrFv5o6t7.jpg") else { return }
+        load(urlImagem)
+    
+    }
+    
+    func load(_ url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self!.imageCapa.image = image
+                    }
+                }
+            }
+        }
     }
 
 }
+//extension UIImageView{
+//    func load(_ url: URL) {
+//        DispatchQueue.global().async { [weak self] in
+//            if let data = try? Data(contentsOf: url) {
+//                if let image = UIImage(data: data) {
+//                    DispatchQueue.main.async {
+//                        self?.image = image
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
