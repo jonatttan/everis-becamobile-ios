b6d4183812f8e6bb05d63e99b56192c1702c8952
Jonattan <jonattan.sousa@gmail.com>
2021-04-28 21:47:11 -0300

    Testes em 59.9%

--- CineFlex/CineFlexUITests/ViewControllerUITests.swift
+++ CineFlex/CineFlexUITests/ViewControllerUITests.swift
@@ -6,20 +6,23 @@
 //
 
 import XCTest
+import Nimble
+import Nimble_Snapshots
+import Quick
 
 class ViewControllerUITests: QuickSpec {
 
-    override func spec() {
-        describe("ViewController") {
-            var tela: UIViewController!
-            beforeEach {
-                tela = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "homeScreen")
-                _ = tela.view
-            }
-            it("Tela Principal") {
-                expect(tela) == recordSnapshot()
-//                expect(tela) == snapshot()
-            }
-        }
-    }
+//    override func spec() {
+//        describe("ViewController") {
+//            var tela: UIViewController!
+//            beforeEach {
+//                tela = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "homeScreen")
+//                _ = tela.view
+//            }
+//            it("Tela Principal") {
+//                expect(tela) == recordSnapshot()
+////                expect(tela) == snapshot()
+//            }
+//        }
+//    }
 }
